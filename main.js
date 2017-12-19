69bcbf3f3924f573fb759bdbdf61330e13d0ca2a
Dor <simhondor@gmail.com>
2017-12-19 15:39:16 +0200

    Reworked case checking algorithim

--- main.js
+++ main.js
@@ -4,26 +4,26 @@ var reservations = {
 }
 // var name = prompt('Please enter the name for your reservation');
 
-function keysToLowerCase(obj) {
-  var keys = Object.keys(obj);
-  var n = keys.length;
-  while (n--) {
-    var key = keys[n]; 
-    if (key !== key.toLowerCase()) { 
-      obj[key.toLowerCase()] = obj[key] 
-      delete obj[key] 
-    }
-  }
-  return (obj);
-}
-keysToLowerCase(reservations);
+// function keysToLowerCase(obj) {
+//   var keys = Object.keys(obj);
+//   var n = keys.length;
+//   while (n--) {
+//     var key = keys[n]; 
+//     if (key !== key.toLowerCase()) { 
+//       obj[key.toLowerCase()] = obj[key] 
+//       delete obj[key] 
+//     }
+//   }
+//   return (obj);
+// }
+// keysToLowerCase(reservations);
 
 
 
 
 var claimReservation = function () {
   var name = document.getElementById("b").value;
-  name = name.toLowerCase()
+  name = name.charAt(0).toUpperCase() + name.substr(1).toLowerCase()
 
   if(reservations[name]){
     if(reservations[name].claimed === false){
