2deace8ae1c38cb4b496d0fb8e70e660586d23f5
Dor <simhondor@gmail.com>
2017-12-19 12:44:52 +0200

    Fixed solution

--- main.js
+++ main.js
@@ -2,9 +2,29 @@ var reservations = {
   "Bob": { claimed: false },
   "Ted": { claimed: true }
 }
-var name = prompt('Please enter the name for your reservation');
+// var name = prompt('Please enter the name for your reservation');
+
+function keysToLowerCase(obj) {
+  var keys = Object.keys(obj);
+  var n = keys.length;
+  while (n--) {
+    var key = keys[n]; 
+    if (key !== key.toLowerCase()) { 
+      obj[key.toLowerCase()] = obj[key] 
+      delete obj[key] 
+    }
+  }
+  return (obj);
+}
+keysToLowerCase(reservations);
+
+
+
 
 var claimReservation = function () {
+  var name = document.getElementById("b").value;
+  name = name.toLowerCase()
+
   if(reservations[name]){
     if(reservations[name].claimed === false){
       alert("Welcome " + name)
@@ -14,9 +34,10 @@ var claimReservation = function () {
     }
 } else {
   reservations[name] = {claimed: true}
-  alert("Your Reservation was claimed")
+    alert("Reservation added under the name: " + name)
+    document.getElementById("output").innerHTML = name
   }
 }
-claimReservation(name);
+// claimReservation(name);
 
 
