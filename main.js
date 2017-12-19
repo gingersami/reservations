5a50c995cfb99d59632003507add4729eebb2e6c
Dor <simhondor@gmail.com>
2017-12-19 11:05:52 +0200

    some answers

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 var reservations = {
-  'Bob': { claimed: false },
-  'Ted': { claimed: true }
+  "Bob": { claimed: false },
+  "Ted": { claimed: true }
 }
-
 var name = prompt('Please enter the name for your reservation');
 
 var claimReservation = function () {
-  // write your code here!
+  if(reservations[name]){
+    if(reservations[name].claimed === false){
+      alert("Welcome " + name)
+    }
+    else if (reservations[name].claimed){
+      alert("Sorry,reservation already claimed")
+    }
+} else {
+  reservations[name] = {claimed: true}
+  alert("Your Reservation was claimed")
+  }
 }
+claimReservation(name);
 
-claimReservation();
+
+
+var tipJar = {
+  coinCount: 20,
+  tip: function () {
+    this.coinCount += 1;
+  },
+  stealCoins: function(num){
+    this.coinCount -= num
+  }
